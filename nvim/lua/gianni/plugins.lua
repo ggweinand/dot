bbf106d952b10765d0b5b80a0f3385e7ec71c0b7
ggweinand <ggweinand@gmail.com>
2024-01-26 17:51:29 -0800

    Closes #1

---
+++ nvim/lua/gianni/plugins.lua
@@ -0,0 +1,26 @@
+return {
+
+	{
+		'nvim-telescope/telescope.nvim', tag = '0.1.5',
+	-- or                              , branch = '0.1.x',
+				dependencies = { 'nvim-lua/plenary.nvim' }
+	},
+
+	{
+		'nvim-treesitter/nvim-treesitter',
+		build = ":TSUpdate"
+	},
+
+
+	-- lsp-zero
+	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+	{'neovim/nvim-lspconfig'},
+	{'hrsh7th/cmp-nvim-lsp'},
+	{'hrsh7th/nvim-cmp'},
+	{'L3MON4D3/LuaSnip'},
+
+	{'ThePrimeagen/harpoon'},
+
+	-- colorscheme
+	{'joshdick/onedark.vim'},
+}
